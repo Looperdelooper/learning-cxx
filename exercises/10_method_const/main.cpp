47a038dc1734f7cf31f3e897030b9639e73b4570
Looperdelooper <aacha@foxmail.com>
2024-07-26 23:37:16 +0800

    modefyww

--- exercises/10_method_const/main.cpp
+++ exercises/10_method_const/main.cpp
@@ -2,21 +2,15 @@
 
 struct Fibonacci {
     int numbers[11];
-
-    Fibonacci(const int (&arr)[11]) {
-        for (int i = 0; i < 11; ++i) {
-            numbers[i] = arr[i];
-        }
-    }
-
+    // TODO: 修改方法签名和实现，使测试通过
     int get(int i) {
-        return numbers[i];
+         return numbers[i];
     }
 };
 
 int main(int argc, char **argv) {
     Fibonacci constexpr FIB{{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55}};
-    ASSERT(FIB.get(index) == 55, "fibonacci(10) should be 55");
-    std::cout << "fibonacci(" << index << ") = " << FIB.get(index) << std::endl;
+    ASSERT(FIB.get(10) == 55, "fibonacci(10) should be 55");
+    std::cout << "fibonacci(10) = " << FIB.get(10) << std::endl;
     return 0;
 }
