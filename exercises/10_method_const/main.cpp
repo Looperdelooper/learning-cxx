b505bb33f94adca63ec9a2797b39479f5757dda6
Looperdelooper <aacha@foxmail.com>
2024-07-26 23:43:49 +0800

    modefylwy1

--- exercises/10_method_const/main.cpp
+++ exercises/10_method_const/main.cpp
@@ -1,7 +1,7 @@
 #include "../exercise.h"
 
 struct Fibonacci {
-    int numbers[11];
+    int numbers[11]={0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
     // TODO: 修改方法签名和实现，使测试通过
     int get(int i) {
          return numbers[i];
@@ -9,7 +9,7 @@ struct Fibonacci {
 };
 
 int main(int argc, char **argv) {
-    Fibonacci constexpr FIB{{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55}};
+    Fibonacci constexpr FIB;
     FIB.get(10);
     ASSERT(FIB.get(10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << FIB.get(10) << std::endl;
