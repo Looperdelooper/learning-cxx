36249ab8eda027ade3fa67a7b9af68a3687fbcdd
Looperdelooper <aacha@foxmail.com>
2024-07-29 23:59:30 +0800

    mod66

--- exercises/17_class_virtual_destruct/main.cpp
+++ exercises/17_class_virtual_destruct/main.cpp
@@ -1,3 +1,4 @@
+
 #include "../exercise.h"
 
 // READ: 静态字段 <https://zh.cppreference.com/w/cpp/language/static>
@@ -48,7 +49,7 @@ int main(int argc, char **argv) {
     ASSERT(B::num_b == 0, "Every B was destroyed");
 
     A *ab = new B;// 派生类指针可以随意转换为基类指针
-    ASSERT(A::num_a == 1, "Fill in the correct value for A::num_a");
+    ASSERT(A::num_a == 2, "Fill in the correct value for A::num_a");
     ASSERT(B::num_b == 1, "Fill in the correct value for B::num_b");
     ASSERT(ab->name() == 'B', "Fill in the correct value for ab->name()");
 
