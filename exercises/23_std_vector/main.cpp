fc3e3cdac30fd3b1416184d4869fe2f50564389b
Looperdelooper <aacha@foxmail.com>
2024-07-31 12:10:50 +0800

    ffh

--- exercises/23_std_vector/main.cpp
+++ exercises/23_std_vector/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv) {
         std::vector<int> vec{1, 2, 3, 4, 5};
         ASSERT(vec.size() == 5, "Fill in the correct value.");
         // THINK: `std::vector` 的大小是什么意思？与什么有关？
-        ASSERT(sizeof(vec) == 5, "Fill in the correct value.");
+        ASSERT(sizeof(vec) == sizeof(std::vector<int>), "Fill in the correct value.");
         int ans[]{1, 2, 3, 4, 5};
         ASSERT(std::memcmp(vec.data(), ans, sizeof(ans)) == 0, "Fill in the correct values.");
     }
